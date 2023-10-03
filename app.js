bc65e261b6607e61b664ccdb0a0ee4582a6c5243
AnasKhan20 <143949011+AnasKhan20@users.noreply.github.com>
2023-10-03 09:46:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+// ARRAYS
+
+// Question 1
+
+// var arr = [];
+
+// console.log(arr)
+
+// Question 3,4,5,6
+
+// var arr = ["Anas",26,true,false,"cat",45];
+
+// console.log(arr[0])
+
+
+// Question 7
+
+// var arr = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"]
+ 
+// for(i=0;i<=arr.length;i++){
+
+//     document.write("Qualification:"+"<br>",arr[i])
+// }
+
+
+// Question 8
+
+// var arrone = ["Michael","John","Tony"];
+// var arrtwo = [320,230,480];
+
+// var totalMark = 500;
+// var percentage = arrtwo / totalMark * 100
+
+// console.log("Score of",arrone[0],"is",arrtwo[0],".",percentage)
+
+// Question 9a
+
+// var userInput = prompt("Enter your colour")
+
+// var arr = [userInput];
+
+// console.log(arr)
+
+//Question 9b
+
+// var userInput = prompt("Enter colour")
+
+// var arr = [userInput]
+
+// console.log(arr)
+
+// Question 9c
+
+
+// var arr = ["red","orange"]
+
+// arr.push("green")
+
+// console.log(arr)
+
+// Question 9d
+
+// var arr = ["red","orange"]
+
+// arr.push("green")
+
+// arr.shift()
+// console.log(arr)
+
+
+// Question 9e
+
+// var arr = ["red","orange"]
+
+// arr.push("green")
+
+// arr.pop()
+// console.log(arr)
+
+
+// Question 10
+
+// var scorearr = [320,230,480,120]
+// scorearrascending = [120,230,320,480]
+// console.log(scorearr[3,1,0,2])
+
+
+// Question 11
+
+// var arr=["karachi","lahore","islamabad","quetta","peshawar"]
+
+// console.log(arr[2,4])
+
